676df3a8861e9a70dd03c3999625e01871bec78c
patelrikin <patelrikin@users.noreply.github.com>
2018-03-31 22:32:16 -0400

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -2,6 +2,6 @@ export function handler(event, context, callback) {
   console.log(event)
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({msg: "Hello, World!"})
+    body: Object.assign({}, JSON.stringify({msg: "Hello, World!"}), {"event": JSON.stringify(event)})
   })
 }
